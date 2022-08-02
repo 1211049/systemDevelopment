8af1822b6fd89b2654d4a6c5832cc7cf91d7ca03
n1211049 <n1211049@PC1211049.jn2.iwasaki.ac.jp>
2022-08-02 10:11:10 +0900

    練習02_2

---
+++ SampleSQL/src/main/java/jp/ac/isc/cloud/UserInsertServlet.java
@@ -0,0 +1,35 @@
+package jp.ac.isc.cloud;
+
+import java.io.*;
+import java.sql.*;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+
+public class UserInsertServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		Connection users = null;
+		try {
+		request.setCharacterEncoding("utf-8");
+		users = DBConnection.openConnection();
+		String id = request.getParameter("insertId");
+		String name = request.getParameter("insertName");
+		String picture = request.getParameter("insertPicture");
+		Statement state = users.createStatement();
+		state.executeUpdate("INSERT INTO user_table VALUE('" + id + "','" + name + "','" +picture + "')");
+		DBConnection.closeConnection(users, state);
+		response.sendRedirect("/select"); 
+		} catch (SQLException e) {
+		e.printStackTrace();
+		}
+		doGet(request, response);
+	}
+
+}
